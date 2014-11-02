75fbdd4525f38709704f0e9de79a3eb63917aeaa
giannis foulidis <giannisf@yandex.ru>
2014-11-02 16:32:59 +0200

    working state

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new \Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
             new QuizCoreBundle()
 
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
